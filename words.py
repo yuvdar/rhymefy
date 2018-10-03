3bbd8e57a601d83aba828d241349f58214e50f71
yuval macbook <yuval@lightricks.com>
2018-10-04 00:15:23 +0300

    word2vec

--- words.py
+++ words.py
@@ -6,5 +6,5 @@ https://drive.google.com/file/d/0B7XkCwpI5KDYNlNUTTlSS21pQmM/edit?usp=sharing
 MODEL = gensim.models.KeyedVectors.load_word2vec_format('./models/GoogleNews-vectors-negative300.bin', binary=True)
 
 
-def get_similar(word, n):
+def get_similar(word, n=10):
     MODEL.most_similar(positive=[word], topn=n)
