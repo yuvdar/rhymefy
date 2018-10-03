47e12bf52279c375cc8e2441907fae98816a233d
yuval macbook <yuval@lightricks.com>
2018-10-03 21:55:55 +0300

    fix find ryhmes

--- find_rhymes.py
+++ find_rhymes.py
@@ -18,10 +18,4 @@ def get_syllables(inp):
 def doTheyRhyme(word1, word2, level):
   # first, we don't want to report 'glue' and 'unglue' as rhyming words
   # those kind of rhymes are LAME
-  if word1.find(word2) == len(word1) - len(word2):
-      return False
-  if word2.find(word1) == len(word2) - len(word1):
-      return False
-
-
   return word1 in rhyme(word2, level)
