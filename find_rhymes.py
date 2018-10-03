c40543554b4d805f5884fb69da0a2ab86adafe4c
yuval macbook <yuval@lightricks.com>
2018-10-03 22:35:11 +0300

    fix find_ryhmes

--- find_rhymes.py
+++ find_rhymes.py
@@ -3,8 +3,10 @@ import nltk
 nltk.download('cmudict')
 """
 
+
 def rhyme(inp, level):
-    syllables = get_syllables(inp)
+    entries = nltk.corpus.cmudict.entries()
+    syllables = [(word, syl) for word, syl in entries if word == inp]
     rhymes = []
     for (word, syllable) in syllables:
         rhymes += [word for word, pron in entries if pron[-level:] == syllable[-level:]]
@@ -17,7 +19,7 @@ def get_syllables(inp):
     return syllables
 
 
-def doTheyRhyme(word1, word2, level):
+def doTheyRhyme(word1, word2, level=1):
   # first, we don't want to report 'glue' and 'unglue' as rhyming words
   # those kind of rhymes are LAME
   return word1 in rhyme(word2, level)
