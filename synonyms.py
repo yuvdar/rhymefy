8006ad3eeb49dce12603b9596fa961a877fa45c4
igor <mrtubis@gmail.com>
2018-10-04 22:08:48 +0300

    all synonyms

--- synonyms.py
+++ synonyms.py
@@ -18,16 +18,31 @@ def syns(word_list):
         word_list = [word_list]
     all_syns = []
     for word in word_list:
-        syns = Word(word).synonyms()
+
+        cWord = Word(word)
+        # syns = cWord.synonyms('all')
+
+        syns = []
+        word_data = cWord.data
+        # if pos is not None:
+        #     word_data = [l for l in word_data if l['partOfSpeech'] == pos]
+        for rel_word_list in word_data:
+            syns.extend([s.word for s in rel_word_list['syn']])
+
         if syns:
             all_syns.extend(syns)
     all_syns.extend(word_list)
     return list(set(all_syns))
 
 
-def find_rhymes(word1, word2):
-    words1 = syns(syns(word1))
-    words2 = syns(syns(word2))
+def find_rhymes(word1, word2, depth1=2, depth2=2):
+    words1 = syns(word1)
+    if depth1==2:
+        words1 = syns(words1)
+
+    words2 = syns(word2)
+    if depth2==2:
+        words2 = syns(words2)
 
     l1 = [last_syllables(w) for w in words1]
     l2 = [last_syllables(w) for w in words2]
@@ -40,10 +55,38 @@ def find_rhymes(word1, word2):
 
     return w1, w2, pd.merge(w1, w2, on='syl')
 
+WORD1,WORD2 = 'peach', 'elbow'
+WORD1,WORD2 = 'midnight', 'blow'
+WORD1,WORD2 = 'me', 'lights'
+WORD1,WORD2 = 'self', 'lights'
+WORD1,WORD2 = 'chair', 'her'
+WORD1,WORD2 = 'say', 'withdraw'
+WORD1,WORD2 = 'sea', 'self'
+WORD1,WORD2 = 'heat', 'go'
+WORD1,WORD2 = 'door', 'stairway'
+WORD1,WORD2 = 'couches', 'drink'
+WORD1,WORD2 = 'seat', 'drink'
+WORD1,WORD2 = 'table', 'pouring'
+WORD1,WORD2 = 'wine', 'smile'
+WORD1,WORD2 = 'look', 'easy'
+WORD1,WORD2 = 'time', 'serious'
+WORD1,WORD2 = 'text', 'you'
 
-w1, w2, m = find_rhymes('peach', 'elbow')
+w1, w2, m = find_rhymes(WORD1,WORD2)
+w1, w2, m = find_rhymes(WORD1,WORD2, depth1=1)
+w1, w2, m = find_rhymes(WORD1,WORD2, depth1=1, depth2=2)
+w1, w2, m = find_rhymes(WORD1,WORD2, depth1=1, depth2=1)
 
 joint_syl = list(set(w1.syl.unique()).intersection(set(w2.syl.unique())))
+fh = open('rhyme_%s_%s.txt'%(WORD1,WORD2), 'w')
+for syl in joint_syl:
+    fh.write('\n')
+    fh.write(syl + ':\n\t')
+    fh.write(", ".join(w1[w1.syl == syl].word.values))
+    fh.write('\n\t')
+    fh.write(", ".join(w2[w2.syl == syl].word.values))
+fh.close()
+
 for syl in joint_syl:
     print syl, ':'
     print w1[w1.syl == syl].word.values
