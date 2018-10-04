926ed2205a74be23c1dba09a5c9561b39864d868
yahelg <yahelg@gmail.com>
2018-10-04 16:57:17 +0300

    pandas from txt

---
+++ gather_songs.py
@@ -0,0 +1,73 @@
+import pandas as pd
+
+
+def get_translated(file_name):
+    lines = open(file_name).readlines()
+    lines = [l for l in lines if len(l)>2]
+
+    # Gather songs
+    songs = []
+    current_song = []
+    for l in lines:
+        if l.find('------') == -1:
+            current_song.append(l)
+        else:
+            songs.append(current_song)
+            current_song = []
+
+    # Take only the last k lines
+    k = 6
+    songs = [song for song in songs if len(song) >= k]
+
+    agg_lines = []
+    for i in range(k):
+        line = [song[-1-i] for song in songs]
+        agg_lines.append(line)
+
+    data = pd.DataFrame({i: agg_lines[i] for i in range(k)})
+    data['translated'] = True
+    return data
+
+
+def filter_song(song):
+    for i in range(len(song)):
+        if song[i].find('this is the end of the song') != -1:
+            return song[:i]
+    return []
+
+
+def get_original(file_name):
+    lines = open(file_name).readlines()
+    lines = [l for l in lines if len(l.strip())>2]
+
+    # Gather songs
+    songs = []
+    current_song = []
+    for l in lines:
+        if l.find('------') == -1:
+            current_song.append(l)
+        else:
+            songs.append(current_song)
+            current_song = []
+
+    # Take only the last k lines
+    k = 6
+    songs = [filter_song(song) for song in songs]
+    songs = [song for song in songs if len(song) >= k]
+
+    agg_lines = []
+    for i in range(k):
+        line = [song[-1-i] for song in songs]
+        agg_lines.append(line)
+
+    data = pd.DataFrame({i: agg_lines[i] for i in range(k)})
+    data['translated'] = False
+    return data
+
+
+def main():
+    data = get_translated('heb_from_csv_s0_e50_en.txt')
+    data2 = get_original('songs_700.txt')
+
+if __name__=='__main__':
+    main()
