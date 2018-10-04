d595ecf2f39c74176200ab1ca4cee9e7dbb3a115
yahelg <yahelg@gmail.com>
2018-10-04 17:50:10 +0300

    Added word2vec

--- gather_songs.py
+++ gather_songs.py
@@ -1,4 +1,16 @@
 import pandas as pd
+from nltk.tokenize import RegexpTokenizer
+import numpy as np
+import gensim
+
+def standardize_text(df, text_field):
+    df[text_field] = df[text_field].str.replace(r"http\S+", "")
+    df[text_field] = df[text_field].str.replace(r"http", "")
+    df[text_field] = df[text_field].str.replace(r"@\S+", "")
+    df[text_field] = df[text_field].str.replace(r"[^A-Za-z0-9(),!?@\'\`\"\_\n]", " ")
+    df[text_field] = df[text_field].str.replace(r"@", "at")
+    df[text_field] = df[text_field].str.lower()
+    return df
 
 
 def get_translated(file_name):
@@ -65,11 +77,33 @@ def get_original(file_name):
     return data
 
 
+def get_average_word2vec(tokens_list, vector, generate_missing=False, k=300):
+    if len(tokens_list)<1:
+        return np.zeros(k)
+    if generate_missing:
+        vectorized = [vector[word] if word in vector else np.random.rand(k) for word in tokens_list]
+    else:
+        vectorized = [vector[word] if word in vector else np.zeros(k) for word in tokens_list]
+    length = len(vectorized)
+    summed = np.sum(vectorized, axis=0)
+    averaged = np.divide(summed, length)
+    return averaged
+
+
 def main():
     data = get_translated('heb_from_csv_s0_e50_en.txt')
     data2 = get_original('songs_700.txt')
     all_data = data.append(data2, ignore_index=True)
     all_data.to_hdf('songs_sample.h5', 'sample')
+    k = 6
+    tokenizer = RegexpTokenizer(r'\w+')
+    word2vec_path = "models/GoogleNews-vectors-negative300.bin"
+    word2vec = gensim.models.KeyedVectors.load_word2vec_format(word2vec_path, binary=True)
+    for i in range(k):
+        standardize_text(all_data, i)
+        all_data['t' + str(i)] = all_data[i].apply(tokenizer.tokenize)
+        all_data['v' + str(i)] = all_data['t' + str(i)].apply(lambda x: get_average_word2vec(x, word2vec))
+
 
 if __name__=='__main__':
     main()
